fb7bbc9dd3ae399f3baa9c01136c5a9529063c8c
Jay Deng <jayd0104@gmail.com>
2019-01-15 19:17:39 -0500

    Created POST endpoint.

--- web.py
+++ web.py
@@ -1,30 +1,53 @@
-from flask import Flask
-from flask_mail import Mail
-from flask_mail import Message
+from flask import Flask, request
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
 with open("credentials.txt", "r") as file:
     username, password = file.read().split()
 
-#TODO: Hide info on github
+#BUG: SSL_WRONG_VERSION
 app.config.update(dict(
     DEBUG = True,
     MAIL_SERVER = 'smtp.gmail.com',
     MAIL_PORT = 587,
-    MAIL_USE_TLS = True,
-    MAIL_USE_SSL = False,
+    # MAIL_USE_TLS = True,
+    MAIL_USE_SSL = True,
     MAIL_USERNAME = username,
     MAIL_PASSWORD = password,
 ))
 
 mail = Mail(app)
 
-#TODO: Look up gmail message specs
 @app.route('/')
+def index():
+    return "/send to send an email"
+
+#TODO: Look up gmail message specs
+@app.route('/send')
 def email():
     msg = Message("Hello",
                   sender=("Jay Deng", "me@example.com"),
                   recipients=["jayd0104@gmail.com"])
     mail.send(msg)
     return
+
+"""
+{
+    "name" : "Name",
+    "email" : "email@address.com",
+    "subject" : "Subject",
+    "message" : "Message body"
+}
+"""
+
+
+@app.route('/post', methods=['POST'])
+def json_example():
+    data = request.get_json()
+    name = data['name']
+    email = data['email']
+    subject = data['subject']
+    message = data['message']
+
+    return "%s, %s, %s, %s" % (name, email, subject, message)
